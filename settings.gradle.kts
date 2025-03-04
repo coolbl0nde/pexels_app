373296aa8142798d75900e1d90c667de3fa7cef7
coolbl0nde <mari.mix19@gmail.com>
2025-03-04 11:34:55 +0300

    feat: setup project and add modules

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pexels App"
+include(":app")
+include(":feature")
+include(":core")
+include(":data_api")
+include(":api_impl")
