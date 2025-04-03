706317cfe1abcca2934bd28bca17f51912040261
coolbl0nde <mari.mix19@gmail.com>
2025-04-03 11:50:14 +0300

    feat: add shimmer

--- feature/src/main/java/com/example/feature/presentation/bookmarks/presentation/component/BookmarksListComponent.kt
+++ feature/src/main/java/com/example/feature/presentation/bookmarks/presentation/component/BookmarksListComponent.kt
@@ -1,15 +1,10 @@
 package com.example.feature.presentation.bookmarks.presentation.component
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.waitForUpOrCancellation
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,18 +15,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -39,7 +27,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.paging.compose.LazyPagingItems
 import coil3.compose.AsyncImage
+import coil3.compose.SubcomposeAsyncImage
 import com.example.core.model.Photo
+import com.example.core.utils.bouncingClickable
+import com.valentinilk.shimmer.shimmer
 
 @Composable
 fun BookmarksListComponent(
@@ -64,21 +55,28 @@ fun BookmarksListComponent(
 
                     Box (
                         modifier = Modifier
-                            .clip(RoundedCornerShape(20))
                             .bouncingClickable {
                                 onPhotoClick(bookmark.id)
-                            },
+                            }
+                            .clip(RoundedCornerShape(20)),
                         contentAlignment = Alignment.Center,
                     ) {
-                        AsyncImage(
+                        SubcomposeAsyncImage (
                             modifier = Modifier
                                 .aspectRatio(aspectRatio)
                                 ,
                             model = bookmark.original,
                             contentDescription = null,
                             contentScale = ContentScale.Crop,
-                            placeholder = ColorPainter(MaterialTheme.colorScheme.surface),
-                            error = ColorPainter(MaterialTheme.colorScheme.surface),
+                            loading = {
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .shimmer()
+                                        .background(MaterialTheme.colorScheme.surface)
+                                )
+                            },
+                            error = { ColorPainter(MaterialTheme.colorScheme.surface) },
                         )
                         Box(
                             modifier = Modifier
@@ -104,37 +102,4 @@ fun BookmarksListComponent(
             }
         },
     )
-}
-
-
-enum class State { Pressed, Idle }
-
-@OptIn(ExperimentalFoundationApi::class)
-fun Modifier.bouncingClickable(
-    onClick: () -> Unit,
-) = composed {
-    var state by remember { mutableStateOf(State.Idle) }
-    val scale by animateFloatAsState(if (state == State.Pressed) 0.70f else 1f)
-
-    this
-        .graphicsLayer {
-            scaleX = scale
-            scaleY = scale
-        }
-        .clickable (
-            interactionSource = remember { MutableInteractionSource() },
-            indication = null,
-            onClick = onClick
-        )
-        .pointerInput(state) {
-            awaitPointerEventScope {
-                state = if (state == State.Pressed) {
-                    waitForUpOrCancellation()
-                    State.Idle
-                } else {
-                    awaitFirstDown(false)
-                    State.Pressed
-                }
-            }
-        }
 }
